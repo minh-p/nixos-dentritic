55ed91ab47fd12f9d031e1d72fb35ff6928ae48d
minh-p <minhisverynormal@gmail.com>
2026-07-30 11:47:50 -0700

    feat: set stateVersion to latest stable

--- modules/hosts/Aurelius/default.nix
+++ modules/hosts/Aurelius/default.nix
@@ -1,7 +1,7 @@
 { inputs, ... }: {
   flake.modules.nixos.Aurelius = {
     nixpkgs.hostPlatform = "x86_64-linux";
-    system.stateVersion = "26.11";
+    system.stateVersion = "26.05";
     _module.args = {
       diskLabels = {
         root = "nixos";
